d56040180b08222de4b33704ea18ba35c4003809
Serhan Apaydın <zerhan@gmail.com>
2017-11-21 16:37:20 +0300

    v0.89.4

--- ClosedXML/Properties/AssemblyVersionInfo.cs
+++ ClosedXML/Properties/AssemblyVersionInfo.cs
@@ -7,6 +7,6 @@ using System.Reflection;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.89.3")]
-[assembly: AssemblyFileVersion("0.89.3")]
-[assembly: AssemblyInformationalVersion("0.89.3")]
+[assembly: AssemblyVersion("0.89.4")]
+[assembly: AssemblyFileVersion("0.89.4")]
+[assembly: AssemblyInformationalVersion("0.89.4")]
