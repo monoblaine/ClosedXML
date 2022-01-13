8d3d8b561ddffda0ad05a0240cfca712c2b147e4
Serhan Apaydın <zerhan@gmail.com>
2022-01-13 22:45:19 +0300

    Add OpenXML format ids

---
+++ ClosedXML/PredefinedFormats.cs
@@ -0,0 +1,149 @@
+//ş
+using System;
+
+namespace ClosedXML {
+    /// <summary>
+    /// https://github.com/closedxml/closedxml/wiki/NumberFormatId-Lookup-Table
+    /// </summary>
+    public static class PredefinedFormats {
+        /// <summary>
+        /// General
+        /// </summary>
+        public static readonly Int32 ID_0 = 0;
+
+        /// <summary>
+        /// 0
+        /// </summary>
+        public static readonly Int32 ID_1 = 1;
+
+        /// <summary>
+        /// 0.00
+        /// </summary>
+        public static readonly Int32 ID_2 = 2;
+
+        /// <summary>
+        /// #,##0
+        /// </summary>
+        public static readonly Int32 ID_3 = 3;
+
+        /// <summary>
+        /// #,##0.00
+        /// </summary>
+        public static readonly Int32 ID_4 = 4;
+
+        /// <summary>
+        /// 0%
+        /// </summary>
+        public static readonly Int32 ID_9 = 9;
+
+        /// <summary>
+        /// 0.00%
+        /// </summary>
+        public static readonly Int32 ID_10 = 10;
+
+        /// <summary>
+        /// 0.00E+00
+        /// </summary>
+        public static readonly Int32 ID_11 = 11;
+
+        /// <summary>
+        /// # ?/?
+        /// </summary>
+        public static readonly Int32 ID_12 = 12;
+
+        /// <summary>
+        /// # ??/??
+        /// </summary>
+        public static readonly Int32 ID_13 = 13;
+
+        /// <summary>
+        /// d/m/yyyy
+        /// </summary>
+        public static readonly Int32 ID_14 = 14;
+
+        /// <summary>
+        /// d-mmm-yy
+        /// </summary>
+        public static readonly Int32 ID_15 = 15;
+
+        /// <summary>
+        /// d-mmm
+        /// </summary>
+        public static readonly Int32 ID_16 = 16;
+
+        /// <summary>
+        /// mmm-yy
+        /// </summary>
+        public static readonly Int32 ID_17 = 17;
+
+        /// <summary>
+        /// h:mm tt
+        /// </summary>
+        public static readonly Int32 ID_18 = 18;
+
+        /// <summary>
+        /// h:mm:ss tt
+        /// </summary>
+        public static readonly Int32 ID_19 = 19;
+
+        /// <summary>
+        /// H:mm
+        /// </summary>
+        public static readonly Int32 ID_20 = 20;
+
+        /// <summary>
+        /// H:mm:ss
+        /// </summary>
+        public static readonly Int32 ID_21 = 21;
+
+        /// <summary>
+        /// m/d/yyyy H:mm
+        /// </summary>
+        public static readonly Int32 ID_22 = 22;
+
+        /// <summary>
+        /// #,##0 ;(#,##0)
+        /// </summary>
+        public static readonly Int32 ID_37 = 37;
+
+        /// <summary>
+        /// #,##0 ;[Red](#,##0)
+        /// </summary>
+        public static readonly Int32 ID_38 = 38;
+
+        /// <summary>
+        /// #,##0.00;(#,##0.00)
+        /// </summary>
+        public static readonly Int32 ID_39 = 39;
+
+        /// <summary>
+        /// #,##0.00;[Red](#,##0.00)
+        /// </summary>
+        public static readonly Int32 ID_40 = 40;
+
+        /// <summary>
+        /// mm:ss
+        /// </summary>
+        public static readonly Int32 ID_45 = 45;
+
+        /// <summary>
+        /// [h]:mm:ss
+        /// </summary>
+        public static readonly Int32 ID_46 = 46;
+
+        /// <summary>
+        /// mmss.0
+        /// </summary>
+        public static readonly Int32 ID_47 = 47;
+
+        /// <summary>
+        /// ##0.0E+0
+        /// </summary>
+        public static readonly Int32 ID_48 = 48;
+
+        /// <summary>
+        /// @
+        /// </summary>
+        public static readonly Int32 ID_49 = 49;
+    }
+}
