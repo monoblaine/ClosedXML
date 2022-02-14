72288ed9624d66dbf3504f810f005b5d4ae111ef
Serhan Apaydın <zerhan@gmail.com>
2022-02-14 17:11:38 +0300

    Change assembly name to ClozedXML

--- ClosedXML/Properties/AssemblyInfo.cs
+++ ClosedXML/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClosedXML")]
+[assembly: AssemblyTitle("ClozedXML")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClosedXML")]
+[assembly: AssemblyProduct("ClozedXML")]
 [assembly: AssemblyCopyright("Copyright © Manuel De Leon 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
